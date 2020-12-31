faa5030f3bd894d382817b032955a57a4f465516
marker/lyl <4017147@qq.com>
2020-12-31 14:08:12 +0800

    新加排序

---
+++ src/main/java/com/marker/datastructure/sorting/InsertSort.java
@@ -0,0 +1,27 @@
+package com.marker.datastructure.sorting;
+
+import java.util.Arrays;
+
+public class InsertSort {
+
+  public static void sort(int[] nums) {
+    for (int i = 0; i < nums.length - 1; i++) {
+      if (nums[i] > nums[i + 1]) {
+        for (int j = i; j >= 0; j--) {
+          if (nums[j] > nums[j + 1]) {
+            int temp = nums[j];
+            nums[j] = nums[j + 1];
+            nums[j + 1] = temp;
+          }
+        }
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    int[] nums = {2, 12, 34, 1, 4, 5, 10, 33};
+    sort(nums);
+    System.out.println(Arrays.toString(nums));
+  }
+
+}
