e8a276b519724871ba82037f2869bc32510881de
marker/lyl <4017147@qq.com>
2021-01-05 18:07:04 +0800

    修改代码

--- src/main/java/com/marker/datastructure/sorting/BubbleSort.java
+++ src/main/java/com/marker/datastructure/sorting/BubbleSort.java
@@ -2,23 +2,21 @@ package com.marker.datastructure.sorting;
 
 import java.util.Arrays;
 
+/**
+ * Title: 交换排序中的冒泡排序 ，一般情形下指的是优化后的冒泡排序，最多进行n-1次比较
+ * Description:因为越大的元素会经由交换慢慢"浮"到数列的顶端(最后位置)，最大的数最后才确定下来，所以称为冒泡排序
+ * 时间复杂度：最好情形O(n)，平均情形O(n^2)，最差情形O(n^2) 空间复杂度：O(1) 稳 定 性：稳定 内部排序(在排序过程中数据元素完全在内存)
+ * 
+ * @describe
+ * @author marker.li lyl
+ * @date 2021/01/05
+ */
 public class BubbleSort {
 
-  public static void sort(int[] nums) {
-    for (int i = 0; i < nums.length; i++) {
-      for (int j = 0; j < nums.length - i - 1; j++) {
-        if (nums[j] > nums[j + 1]) {
-          int temp = nums[j + 1];
-          nums[j + 1] = nums[j];
-          nums[j] = temp;
-        }
-      }
-    }
-  }
-
-  public static void sort2(int[] nums) {
-    for (int i = 0; i < nums.length; i++) {
-      for (int j = 0; j < nums.length - 1 - i; j++) {
+  public void bubbleSort(int[] nums) {
+    int n = nums.length;
+    for (int i = 0; i < n - 1; i++) {
+      for (int j = i; j < n - 1 - i; j++) {
         if (nums[j] > nums[j + 1]) {
           int temp = nums[j];
           nums[j] = nums[j + 1];
@@ -28,9 +26,35 @@ public class BubbleSort {
     }
   }
 
+  public void optimizeBubbleSort(int[] nums) {
+    int n = nums.length;
+    for (int i = 0; i < n - 1; i++) {
+      boolean exchange = false;
+      for (int j = i; j < n - 1 - i; j++) {
+        if (nums[j] > nums[j + 1]) {
+          int temp = nums[j];
+          nums[j] = nums[j + 1];
+          nums[j + 1] = temp;
+          exchange = true;
+        }
+      }
+      if (!exchange) {
+        return;
+      }
+    }
+  }
+
   public static void main(String[] args) {
-    int[] nums = {2, 12, 34, 1, 4, 5, 10, 33};
-    sort2(nums);
+    BubbleSort bubbleSort = new BubbleSort();
+//    int[] nums = {2, 12, 34, 3, 4, 5, 10, 33};
+//    bubbleSort.bubbleSort(nums);
+//    System.out.println(Arrays.toString(nums));
+    System.out.println("\n----------------------\n");
+    System.out.println("优化冒泡排序 ： ");
+    int[] nums = {1, 2, 3, 4, 5, 8, 7, 6};
+    System.out.println("原数组 ： " + Arrays.toString(nums));
+    bubbleSort.optimizeBubbleSort(nums);
     System.out.println(Arrays.toString(nums));
   }
+
 }
--- src/main/java/com/marker/datastructure/sorting/InsertSort.java
+++ src/main/java/com/marker/datastructure/sorting/InsertSort.java
@@ -6,38 +6,43 @@ public class InsertSort {
 
   public static void sort(int[] nums) {
     for (int i = 0; i < nums.length - 1; i++) {
-      if (nums[i] > nums[i + 1]) {
-        for (int j = i; j >= 0; j--) {
-          if (nums[j] > nums[j + 1]) {
-            int temp = nums[j];
-            nums[j] = nums[j + 1];
-            nums[j + 1] = temp;
-          }
+      for (int j = i + 1; j > 0; j--) {
+        if (nums[j - 1] > nums[j]) {
+          int temp = nums[j - 1];
+          nums[j - 1] = nums[j];
+          nums[j] = temp;
         }
       }
     }
   }
 
-  public static void sort2(int[] nums) {
-    for (int i = 0; i < nums.length - 1; i++) {
-      if (nums[i] > nums[i + 1]) {
-        for (int j = i; j >= 0; j--) {
-          if (nums[j] > nums[j + 1]) {
-            int temp = nums[j];
-            nums[j] = nums[j + 1];
-            nums[j + 1] = temp;
+
+  public static void optimizeSort(int[] nums) {
+    for (int i = 1; i < nums.length; i++) {
+      int left = 0, mind, right = i - 1;
+      int temp = nums[i];
+      if (temp < nums[right]) {
+        while (left <= right) {
+          mind = (left + right) / 2;
+          if (nums[mind] < temp) {
+            left = mind + 1;
+          } else if (nums[mind] > temp) {
+            right = mind - 1;
+          } else {
+            left = left + 1;
           }
         }
+        for (int j = i; j > left; j--) {
+          nums[j] = nums[j - 1];
+        }
+        nums[left] = temp;
       }
     }
   }
-
 
   public static void main(String[] args) {
-    int[] nums = {2, 12, 34, 1, 4, 5, 10, 33};
-    sort2(nums);
+    int[] nums = {12, 4, 13, 25, 92, 11, 9, 3, 2};
+    InsertSort.sort(nums);
     System.out.println(Arrays.toString(nums));
-//    2, 12, 34, 1, 4, 5, 10, 33
   }
-
 }
