20fc34365ce0435d0e36f2132d754ea80bcd2d85
marker/lyl <4017147@qq.com>
2021-01-08 18:03:07 +0800

    个好久好久

--- src/main/java/com/marker/datastructure/sorting/RadixSort.java
+++ src/main/java/com/marker/datastructure/sorting/RadixSort.java
@@ -14,24 +14,6 @@ import java.util.Arrays;
  * @date 2021/01/08
  */
 public class RadixSort {
-  /**
-   * @description 分配 + 收集
-   * @author rico
-   * @created 2017年5月21日 下午9:25:52
-   * @param target 待排序数组
-   * @param r 基数
-   * @param d 元素的位数
-   * @param n 待排序元素个数
-   * @return
-   */
-  public static int[] radixSort(int[] target, int r, int d, int n) {
-    int max = maxNum(target);
-    int exp;
-    for (exp = 1; max / exp > 0; exp *= 10) {
-
-    }
-    return target;
-  }
 
   public static int[] countSort(int a[], int exp) {
     int i, buckets[] = new int[10];
@@ -64,12 +46,7 @@ public class RadixSort {
   }
 
   public static void main(String[] args) {
-//    System.out.println("\n----------------------\n");
-//    System.out.println("基数排序 ： ");
     int[] target8 = {53, 3, 542, 748, 14, 214, 154, 63, 616};
-//    System.out.println("原数组 ： " + Arrays.toString(target8));
-//    RadixSort.radixSort(target8, 10, 3, target8.length);
-//    System.out.println(Arrays.toString(target8));
 
     int exp; // 指数。当对数组按各位进行排序时，exp=1；按十位进行排序时，exp=10；...
     int max = maxNum(target8); // 数组a中的最大值
